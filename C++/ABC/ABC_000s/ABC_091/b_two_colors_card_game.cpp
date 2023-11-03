b9a2f3498d366b24c9e7ebeb2517a016513ddb35
eyamagishi <etsuro.yamagishi0625@gmail.com>
2023-11-04 01:37:19 +0900

    add: ABC_091のB問題を追加

---
+++ C++/ABC/ABC_000s/ABC_091/b_two_colors_card_game.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n;
+  vector<string> S(n);
+  for(int i = 0;i < n;i++) {cin >> S[i];}
+  cin >> m;
+  vector<string> T(m);
+  for(int i = 0;i < m;i++) {cin >> T[i];}
+
+  int max = 0;
+  for(int i = 0;i < n;i++) {
+    int cnt = 0;
+    string str = S[i];
+    for(int j = 0;j < n;j++) {
+      if(S[j] == str) {cnt++;}
+    }
+    for(int j = 0;j < m;j++) {
+      if(T[j] == str) {cnt--;}
+    }
+    if(max < cnt) {max = cnt;}
+  }
+  cout << max << endl;
+}
