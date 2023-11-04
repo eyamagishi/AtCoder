d923adf91f4315d1bda53d0f89b552db146de4c8
eyamagishi <etsuro.yamagishi0625@gmail.com>
2023-11-04 23:16:36 +0900

    add: ABC327のB問題

---
+++ C++/ABC/ABC_300s/ABC_327/b_a^a.cpp
@@ -0,0 +1,18 @@
+// オーバーフロー
+// pow関数の浮動小数点型による誤差
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long int b;
+  cin >> b;
+  for (int a = 1; a <= 15; a++) {
+    long long int x = 1;
+    for (int i = 0; i < a; i++) x *= a;
+    if (x == b) {
+      cout << a << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+}
