2ac26d5018346a6fa689b2035fc6dd97358763fa
eyamagishi <etsuro.yamagishi0625@gmail.com>
2023-11-04 22:50:08 +0900

    add: ABC327のA問題

---
+++ C++/ABC/ABC_300s/ABC_327/a_ab.cpp
@@ -0,0 +1,15 @@
+// 基本的な全探索
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  string S;
+  cin >> N >> S;
+  bool yes = false;
+  for(int i = 0; i < N - 1; i++) {
+    if (S[i] == 'a' && S[i + 1] == 'b') yes = true;
+    if (S[i] == 'b' && S[i + 1] == 'a') yes = true;
+  }
+  cout << (yes ? "Yes" : "No") << endl;
+}
