9313dd140154264b588a5fc82d221b3b4dcde952
eyamagishi <e.yamagishi0625@gmail.com>
2023-11-12 02:36:49 +0900

    fix: ABC079のA問題修正

--- C++/ABC/ABC_000s/ABC_079/a_good_integer.cpp
+++ C++/ABC/ABC_000s/ABC_079/a_good_integer.cpp
@@ -5,13 +5,14 @@ int main() {
   int n;
   string ans;
   cin >> n;
+
   int n1000 = n/1000,
       n100 = n%1000/100,
       n10 = n%100/10,
       n1 = n%10;
-  if(n1000 != n100 && n1000 != n10) {ans = "No";}
-  else if(n100 != n10 && n100 != n1) {ans = "No";}
-  else {ans = "Yes";}
+  ans = "No";
+  if((n1000 == n100 && n1000 == n10) || (n100 == n10 && n10 == n1)) ans = "Yes";
+
   cout << ans << endl;
   return 0;
 }
