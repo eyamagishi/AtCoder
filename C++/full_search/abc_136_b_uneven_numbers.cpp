78418ec2b22eb4e6f62f0aa56e28e8b0462e4b27
eyamagishi <e.yamagishi0625@gmail.com>
2023-12-24 12:23:21 +0900

    add: 全探索用ディレクトリに追加

--- C++/full_search/abc_136_b_uneven_numbers.cpp
+++ C++/full_search/abc_136_b_uneven_numbers.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main() {
-  int n;
+  int n, ans = 0;;
   cin >> n;
-
-  int cnt = 0;
-  for (int i = 0; i < n; i++) {
-    
+  for (int i = 1; i <= n; i++) {
+    int cnt = 0, tmp = i;
+    while (tmp != 0) {
+      cnt++;
+      tmp /= 10;
+    }
+    if (cnt % 2 != 0) ans++;
   }
+
+  cout << ans << endl;
 }
