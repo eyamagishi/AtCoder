64c354f8ee65656d7641f94fd339eb4c741ffca8
e2 <e.yamagishi0625@gmail.com>
2024-05-01 21:41:37 +0900

    fix: ディレクトリ配置修正

---
+++ C++/ABC/ABC_300s/ABC_350/a_past_abcs.cpp
@@ -0,0 +1,39 @@
+/**
+  * filename: a_past_abcs.cpp
+  * author: teniwoha
+  * X: @RyutaUrushi
+  * created: 05/01/2024 12:48:07
+  * result: 
+  **/
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+#define bit(n) (1LL<<(n))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i,a) for(auto &i:a)
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+
+
+int main() {
+  string s;
+  cin >> s;
+  int str_num =  stoi(s.substr(3, 3));
+  
+  cout << str_num << endl;
+}
