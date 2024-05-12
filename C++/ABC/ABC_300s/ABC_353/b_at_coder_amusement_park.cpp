8e91663b6748636f41a543208ba4778d2b10f9e8
e2 <e.yamagishi0625@gmail.com>
2024-05-12 17:45:59 +0900

    add: ABC_353_B問題_AC

---
+++ C++/ABC/ABC_300s/ABC_353/b_at_coder_amusement_park.cpp
@@ -0,0 +1,53 @@
+/**
+  * filename: b_at_coder_amusement_park.cpp
+  * author: teniwoha
+  * X: @RyutaUrushi
+  * created: 05/12/2024 16:58:40
+  * result: 
+  **/
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+#define bit(n) (1LL << (n))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for(auto &i:a)
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, k;
+  cin >> n >> k;
+
+  int cnt = 0, store = k;
+  rep (i, 0, n) {
+    int a;
+    cin >> a;
+    if (store < a) {
+      cnt++;
+      store = k;
+    }
+    store -= a;
+  }
+  cnt++;
+
+  cout << cnt << endl;
+  return 0;
+}
