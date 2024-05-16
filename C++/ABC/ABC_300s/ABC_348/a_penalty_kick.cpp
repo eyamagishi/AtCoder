56eb003823d0bdac2ad1fb351044e132e25e02fa
e2 <e.yamagishi0625@gmail.com>
2024-05-16 18:38:50 +0900

    add: ABC_348_A問題_AC

---
+++ C++/ABC/ABC_300s/ABC_348/a_penalty_kick.cpp
@@ -0,0 +1,45 @@
+/**
+  * filename: a_penalty_kick.cpp
+  * author: teniwoha
+  * X: @RyutaUrushi
+  * created: 05/16/2024 18:33:53
+  * result: AC
+  **/
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+#define bit(n) (1LL << (n))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for(auto &i:a)
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  rep (i, 1, n + 1) {
+    if (i % 3) cout << 'o';
+    else cout << 'x';
+  }
+
+  cout << '\n';
+  return 0;
+}
