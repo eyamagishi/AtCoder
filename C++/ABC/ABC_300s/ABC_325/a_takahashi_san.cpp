af26d246cfd7f48be760272a1bc9cfcb220d7ceb
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-11 09:14:47 +0900

    add: ABC_325_A - Takahashi san_AC

---
+++ C++/ABC/ABC_300s/ABC_325/a_takahashi_san.cpp
@@ -0,0 +1,64 @@
+/**
+    * filename: a_takahashi_san.cpp
+    * author: teniwoha
+    * X: @RyutaUrushi
+    * created: 06/11/2024 09:10:24
+    * result: AC
+    **/
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+#define bit(n) (1LL << (n))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for(auto &i:a)
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string s, t;
+    cin >> s >> t;
+    cout << s << ' ' << "san\n";
+
+    return 0;
+}
+
+// A - Takahashi san 解説 by yuto1115
+
+// AtCoder をはじめたばかりで何をしたらよいか分からない方は、
+// まずは practice contest の問題A「Welcome to AtCoder」を解いてみてください。
+// 基本的な入出力の方法が載っています。
+// また、プログラミングコンテストの問題に慣れていない方は、
+// AtCoder Beginners Selection の問題をいくつか解いてみることをおすすめします。
+
+// 文字列の入出力に関する基本的な問題です。問題文で指示されている通りに文字列を入出力してください。
+
+// 以下に C++ における実装例を載せています。これら以外の言語を使用している方は、前述の practice contest 等を参考にしてください。
+
+/* 実装例 (C++) : */
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// int main() {
+//     string s, t;
+//     cin >> s >> t;
+//     cout << s << " san" << endl;
+// }
