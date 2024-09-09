ccd72a2a5006be4869bebc29ea8c823512770732
eyamagishi <e.yamagishi0625@gmail.com>
2024-09-09 18:25:56 +0900

    add

---
+++ C++/ABC/ABC_300s/ABC_368/a_cut.cpp
@@ -0,0 +1,49 @@
+/**
+ * filename: a_cut.cpp
+ * author: teniwoha
+ * X: @RyutaUrushi
+ * created: 08/31/2024 20:50:01
+ * result: AC
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+#define bit(n) (1LL << (n))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    rep (i, 0, n) cin >> a.at(i);
+
+    rep (i, 0, n) {
+        if (i != 0) cout << ' ';
+        int index = (i + n - k) % n;
+        cout << a.at(index);
+    }
+    cout << '\n';
+
+    return 0;
+}
