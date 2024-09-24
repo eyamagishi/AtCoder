ac0f66e0ba1bd23db50397b2f97085d44577c8b6
eyamagishi <e.yamagishi0625@gmail.com>
2024-09-24 21:04:16 +0900

    add: ABC_372_A - delete ._AC

---
+++ C++/ABC/ABC_300s/ABC_372/A - delete ..cpp
@@ -0,0 +1,44 @@
+/**
+ * filename: A - delete ..cpp
+ * author: teniwoha
+ * X: @RyutaUrushi
+ * created: 09/24/2024 21:01:37
+ * result: AC
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+#define bit(n) (1LL << (n))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string s;
+    cin >> s;
+    rep (i, 0, SZ(s)) {
+        if (s[i] != '.') cout << s[i];
+    }
+    cout << '\n';
+
+    return 0;
+}
