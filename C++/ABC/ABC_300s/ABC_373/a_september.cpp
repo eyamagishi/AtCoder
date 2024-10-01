fbc05e74191094a826ebd98a52631a81be4c3b5b
eyamagishi <e.yamagishi0625@gmail.com>
2024-10-01 20:37:02 +0900

    add: ABC_373_A - September_AC

---
+++ C++/ABC/ABC_300s/ABC_373/a_september.cpp
@@ -0,0 +1,45 @@
+/**
+ * filename: a_september.cpp
+ * author: teniwoha
+ * X: @RyutaUrushi
+ * created: 10/01/2024 20:32:29
+ * result: AC
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+#define bit(n) (1LL << (n))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(obj) (obj).begin(), (obj).end()
+#define rall(obj) (obj).rbegin(), (obj).rend()
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int cnt = 0;
+    rep (i, 0, 12) {
+        string s;
+        cin >> s;
+        if (SZ(s) == i + 1) cnt++;
+    }
+
+    cout << cnt << '\n';
+    return 0;
+}
